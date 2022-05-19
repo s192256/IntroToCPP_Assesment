67c24f5b09b16fff03731c3a777955c27bf24d11
s192256 <s192256@students.aie.edu.au>
2022-05-19 15:39:09 +0930

    Made the ball move

--- IntroToCPP_Assesment/RetroGame/RetroGame.cpp
+++ IntroToCPP_Assesment/RetroGame/RetroGame.cpp
@@ -1,6 +1,117 @@
+#include "raylib.h"
 #include <iostream>
+#include <random>
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    int screenWidth = 800;
+    int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "PONG");
+
+    SetTargetFPS(60);
+
+    float ballX = screenWidth / 2;
+    float ballY = screenHeight / 2;
+
+    std::random_device r;
+    std::default_random_engine generator{ r() };
+    std::uniform_real_distribution<float> distribution(-1.0, 1.0);
+    float rand = distribution(generator);
+    float rand2 = distribution(generator);
+    
+
+    float ballXrand = rand;
+    float ballYrand = rand2;
+
+    while (ballXrand == 0)
+    {
+        ballXrand = distribution(generator);;
+    }
+    while (ballYrand == 0)
+    {
+        ballYrand = distribution(generator);
+    }
+
+    float ballXChange = 5 * ballXrand;
+    float ballYChange = 5 * ballYrand;
+
+    bool directionChange = false;
+
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        if (ballXChange < 2.5 && ballXChange >= 0)
+            ballXChange = 2.5;
+        if (ballXChange > -2.5 && ballXChange < 0)
+            ballXChange = -2.5;
+        if (ballYChange < 2.5 && ballYChange >= 0)
+            ballYChange = 2.5;
+        if (ballYChange > -2.5 && ballYChange < 0)
+            ballYChange = -2.5;
+
+        if (ballY > screenHeight - 10)
+        {
+            if (directionChange == false)
+            {
+                directionChange = true;
+                ballYChange = ballYChange * -1;
+            }
+        }
+        else if (ballY < 10)
+        {
+            if (directionChange == false)
+            {
+                directionChange = true;
+                ballYChange = ballYChange * -1;
+            }
+        }
+
+        ballX += (ballXChange);
+        ballY += (ballYChange);
+
+        
+        // Draw
+        // 
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+
+
+        DrawText("|", screenWidth / 2 - 1, 7.5, 20, WHITE);
+        DrawText("|", screenWidth / 2 - 1, 37.5, 20, WHITE);
+        DrawText("|", screenWidth / 2 - 1, 67.5, 20, WHITE);
+        DrawText("|", screenWidth / 2 - 1, 97.5, 20, WHITE);
+        DrawText("|", screenWidth / 2 - 1, 127.5, 20, WHITE);
+        DrawText("|", screenWidth / 2 - 1, 157.5, 20, WHITE);
+        DrawText("|", screenWidth / 2 - 1, 187.5, 20, WHITE);
+        DrawText("|", screenWidth / 2 - 1, 217.5, 20, WHITE);
+        DrawText("|", screenWidth / 2 - 1, 247.5, 20, WHITE);
+        DrawText("|", screenWidth / 2 - 1, 277.5, 20, WHITE);
+        DrawText("|", screenWidth / 2 - 1, 307.5, 20, WHITE);
+        DrawText("|", screenWidth / 2 - 1, 337.5, 20, WHITE);
+        DrawText("|", screenWidth / 2 - 1, 367.5, 20, WHITE);
+        DrawText("|", screenWidth / 2 - 1, 397.5, 20, WHITE);
+        DrawText("|", screenWidth / 2 - 1, 427.5, 20, WHITE);
+
+        DrawCircle(ballX, ballY, 10, WHITE);
+
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------   
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
 }
